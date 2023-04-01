0a8419e03033cce5d4d181b5cd1b26283e0bcff8
abdussalam-mustapha <abdussalammustapha07@gmail.com>
2023-04-01 14:30:11 -0700

    bookig app frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import { Route, Routes } from "react-router-dom"
 import './App.css';
+
+import AuthPage from "./pages/Auth";
+import BookingsPage from "./pages/Bookings";
+import EventsPage from "./pages/Events";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/auth" element={<AuthPage/>} />
+      <Route path="/events" element={<EventsPage/>} />
+      <Route path="/bookings" element={<BookingsPage/>} />
+    </Routes>
   );
 }
 
---
+++ src/components/Navigation/MainNavigation.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const MainNavigation = (props) => {
+  return (
+    <header>
+        <div className='main-navigation__logo'>
+            <h1>bookit</h1>
+        </div>
+        <nav className='main-navigation__items'>
+            <ul>
+                <li></li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default MainNavigation
